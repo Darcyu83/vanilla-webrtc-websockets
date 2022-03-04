c74722f860320f5629e66d4b6f4b516c04a9dc3c
yuds <darcy.u83@gmail.com>
2022-03-04 15:56:25 +0900

    Set up Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+
+//Configuring view engine which should be pug
+app.set("view engine", "pug");
+
+//Telling express where are going to be our template
+app.set("views", __dirname + "/views");
+
+//Creating URL public and share some files with the users
+app.use("/public", express.static(__dirname + "/public"));
+
+//Creating only route handler which is rendering home.pug
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleListen = () => console.log("Listening on http://localhost:3000/");
+app.listen(3000);
