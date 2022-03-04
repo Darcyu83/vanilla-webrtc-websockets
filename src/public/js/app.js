bb90dc17921fb3b89aefd670d1cf27ca1823e662
yuds <darcy.u83@gmail.com>
2022-03-04 18:05:35 +0900

    add websocket connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Browser ✔");
+});
+
+socket.addEventListener("message", (msg) => {
+  console.log("Message from Server ===", msg.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server");
+});
+
+setTimeout(() => {
+  socket.send("hello Backend from browser 🧡");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-
+import http from "http";
+import WebSocket from "ws";
 const app = express();
 
 //Configuring view engine which should be pug
@@ -16,5 +17,33 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-const handleListen = () => console.log("Listening on http://localhost:3000/");
-app.listen(3000);
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listening on ws://localhost:3000/");
+// app.listen(3000, handleListen);
+
+// requestListener => http + wss on port 3000
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const handleConnection = (socket, req) => {
+  console.log(
+    "socket in server.js means browser just connected to server ",
+    socket
+  );
+  console.log("req", req);
+};
+wss.on("connection", (socket) => {
+  //   console.log(
+  //     "socket in server.js means browser just connected to server ",
+  //     socket
+  //   );
+  console.log("Connected to Sever ✔");
+  socket.on("close", () => console.log("Disconnected from browser"));
+  socket.on("message", (message) =>
+    console.log("Message from browser", message.toString("utf8"))
+  );
+  socket.send("Hello from server socket");
+});
+
+server.listen(3000, handleListen);
